648c4d64f17685b6584649a12fb00e29ab3adac2
REYNALDO ADRIAN CAUCHE VILCA <reynaldo.cauche@hatun.com.pe>
2023-11-05 21:08:50 -0500

    subir archivos

---
+++ src/components/Article.jsx
@@ -0,0 +1,22 @@
+export const Article = ({
+  img,
+  number,
+  title,
+  text
+}) => {
+  return (
+    <article className='flex h-[162px] md:w-[343px]'>
+      <div className='w-[100px] flex-none'>
+        <img src={img} alt="" />
+      </div>
+      <div className='pl-6'>
+        <p className='text-GrayishBlue text-3x1 mb-[18px] font-bold'>{number}</p>
+        <h2 className='font-bold mb-[18px] hover:text-SoftOrange cursor-pointer'>{title}</h2>
+        <p className='text-DarkGrayishBlue'>{text}</p>
+      </div>
+    </article>
+  )
+}
+
+// Reviving Retro PCs
+//   What happens when old PCs are given modern upgrades?
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import burgerMenu from '../assets/images/icon-menu.svg'
+
+export const NavBar = () => {
+  return (
+    <>    
+      <ul className="hidden sm:flex text-[18px] sm:w-[438px] sm:place-content-around sm:text-[16px] sm:items-center">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Popular</a>
+        </li>
+        <li>
+          <a href="#">Trending</a>
+        </li>
+        <li>
+          <a href="#">Categories</a>
+        </li>
+      </ul>
+      {/* Altura de 16, ancho de 40 px, se multiplica por 4 */}
+      <img className='w-10 h-4 cursor-pointer sm:hidden' src={burgerMenu} alt="Menu hamburguesa" />
+    </>
+  )
+}
